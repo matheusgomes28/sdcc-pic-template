192184e730d7cdc132190aec07a2a7720184f1f3
matheusgomes28 <matheusgarcia28@gmail.com>
2021-04-04 23:13:46 +0100

    Slowly adding the cmake to find the compiler and set the flags.

--- modules/Platform/SDCC_PIC_16F877A.cmake
+++ modules/Platform/SDCC_PIC_16F877A.cmake
@@ -1,4 +1,5 @@
 # Check if the shit exists
+
 message("!!! The value of root is ${SDCC_ROOT}")
 if (NOT DEFINED SDCC_ROOT)
   message(FATAL_ERROR
@@ -7,8 +8,23 @@ if (NOT DEFINED SDCC_ROOT)
 endif()
 
 # Finding the compilers
-find_program(CMAKE_C_COMPILER
+find_program(SDCC_COMPILER
   sdcc
   PATHS ${SDCC_ROOT}
   PATH_SUFFIXES "bin"
   DOC "path to the SDCC C compiler.")
+
+if (SDCC_COMPILER)
+  set(CMAKE_C_COMPILER "${SDCC_COMPILER}" CACHE STRING "compiler" FORCE)
+endif()
+
+# PIC16F77A specific
+set(CMAKE_C_OUTPUT_EXTENSION ".o")
+set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
+
+
+set(CMAKE_C_FLAGS  "-mpic14")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -p16f877a")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --use-non-free")
+
+message("!!!! The program found is ${SDCC_COMPILER}")
--- modules/toolchain.cmake
+++ modules/toolchain.cmake
@@ -20,3 +20,20 @@ endif()
 if (NOT MICROCHIP_FAMILY STREQUAL "pic16" AND NOT MICROCHIP_MODEL STREQUAL "16f877a")
   message(FATAL_ERROR "Settings not supported. Please drop a request.")
 endif()
+
+if (SDCC_ROOT)
+  set(CMAKE_SYSROOT ${SDCC_ROOT})
+  list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES
+          SDCC_ROOT
+          CMAKE_SYSROOT)
+endif()
+
+# Cache those variables
+set(SDCC_ROOT "${SDCC_ROOT}"
+        CACHE INTERNAL "Root directory of SDCC installation")
+
+set(MICROCHIP_FAMILY "${MICROCHIP_FAMILY}"
+        CACHE INTERNAL "Family of the chip to compile for")
+
+set(MICROCHIP_MODEL "${MICROCHIP_MODEL}"
+        CACHE INTERNAL "Model of the chip to compile for")
