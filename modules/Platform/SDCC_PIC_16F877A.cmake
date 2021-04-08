93abd185b32c411a461ff50f88bcd6ad1433a19e
matheusgomes28 <matheusgarcia28@gmail.com>
2021-04-08 19:12:30 +0100

    CLion includes the directories in INCLUDE_DIRECTORIES.

--- modules/Platform/SDCC_PIC_16F877A.cmake
+++ modules/Platform/SDCC_PIC_16F877A.cmake
@@ -1,11 +1,16 @@
 # Check if the shit exists
 
-message("!!! The value of root is ${SDCC_ROOT}")
 if (NOT DEFINED SDCC_ROOT)
   message(FATAL_ERROR
     "SDCC_ROOT is not defined. Please set this variable e.g.\n"
     "cmake -DSDCC_ROOT=\"C:/Program Files/sdcc\"")
 endif()
+
+set(CMAKE_SYSROOT "${SDCC_ROOT}")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # Finding the compilers
 find_program(SDCC_COMPILER
@@ -15,16 +20,22 @@ find_program(SDCC_COMPILER
   DOC "path to the SDCC C compiler.")
 
 if (SDCC_COMPILER)
-  set(CMAKE_C_COMPILER "${SDCC_COMPILER}" CACHE STRING "compiler" FORCE)
+  set(CMAKE_C_COMPILER "${SDCC_COMPILER}" CACHE STRING "C compiler" FORCE)
 endif()
 
 # PIC16F77A specific
 set(CMAKE_C_OUTPUT_EXTENSION ".o")
 set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
 
-
 set(CMAKE_C_FLAGS  "-mpic14")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -p16f877a")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --use-non-free")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --disable-warning 1")
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --Werror")
 
-message("!!!! The program found is ${SDCC_COMPILER}")
+if (SDCC_ROOT)
+  set(SDCC_NON_FREE_INCLUDES "${SDCC_ROOT}/non-free/include")
+  set(SDCC_INCLUDES "${SDCC_ROOT}/include")
+else()
+  message(FATAL_ERROR "SDCC compiler was not found in the root given.")
+endif()
